be083e572aa5a2a70932950c0ed62a5e622014d7
turlodales <paulturley_2@hotmail.com>
2020-04-01 18:28:09 +0100

    Update partialzip.c

--- tools/partialzip.c
+++ tools/partialzip.c
@@ -1,23 +1,23 @@
-/**
-  * libpartialzip-1.0 - partialzip.c
-  * Copyright (C) 2010 David Wang
-  *
-  * Modified by:
-  * Copyright (C) 2010-2013 Joshua Hill
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
+/*
+ * partialzip.c
+ * Copyright © 2010 David Wang
+ *
+ * Modified by:
+ * Copyright © 2010-2013 Joshua Hill
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,11 +25,9 @@
 #include <libcrippy-1.0/libcrippy.h>
 #include <libpartialzip-1.0/libpartialzip.h>
 
-
 void download_callback(partialzip_t* info, partialzip_file_t* file, size_t progress) {
 	print_progress(progress, NULL);
 }
-
 
 void usage() {
 	printf("usage: partialzip <zip> <path> <output>\n");
